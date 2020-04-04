627926ad3b1573cd777e0ec4b5d4461332be5243
Dulio Denis <dulio.denis@yahoo.com>
2020-04-04 11:46:55 -0400

    Implemented calculating a winner in the handleClick method

--- tic-tac-toe/src/components/Game.js
+++ tic-tac-toe/src/components/Game.js
@@ -17,8 +17,13 @@ export default class Game extends Component {
     const history = this.state.history.slice(0, this.state.stepNumber+1);
     const current = history[history.length-1];
     const boxes = current.boxes.slice();
+    
+    const winner = calculateWinner(boxes);
+    if (winner || boxes[i]) {
+      return;
+    }
+    
     boxes[i] = this.state.xIsNext ? 'X' : 'O';
-
     this.setState({
       history: history.concat({
         boxes: boxes
@@ -42,4 +47,29 @@ export default class Game extends Component {
       </div>
     )
   }
+}
+
+// -------------------------------
+// Helper function 
+// -------------------------------
+function calculateWinner(boxes) {
+  const winPatterns = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
+
+  for (let i=0; i < winPatterns.length; i++) {
+    const [a,b,c] = winPatterns[i];
+    if (boxes[a] && boxes[a] === boxes[b] && 
+                    boxes[b] === boxes[c]) {
+      return boxes[a];
+    }
+  }
+  return null;
 }
